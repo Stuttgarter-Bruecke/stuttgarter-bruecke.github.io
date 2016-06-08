031f22636a6b63c76d57bc692800db29a17e99cb
Cristobal Tapia Camu <crtapia@gmail.com>
2016-06-08 10:22:43 +0200

    Javascript für die startpage wurde sepparat geschrieben

---
+++ javascripts/startpage_actions.js
@@ -0,0 +1,25 @@
+window.onload = function() {
+  var zone = document.getElementById('zone3-1');
+  zone.setAttribute('opacity', '0.1');
+  var zone = document.getElementById('zone3-2');
+  zone.setAttribute('opacity', '0.1');
+  var zone = document.getElementById('zone3-3');
+  zone.setAttribute('opacity', '0.1');
+  var zone = document.getElementById('zone2-1');
+  zone.setAttribute('opacity', '0.1');
+  var zone = document.getElementById('zone2-2');
+  zone.setAttribute('opacity', '0.1');
+  var zone = document.getElementById('zone2-3');
+  zone.setAttribute('opacity', '0.1');
+  var zone = document.getElementById('zone1-1');
+  zone.setAttribute('opacity', '0.1');
+  var zone = document.getElementById('zone1-2');
+  zone.setAttribute('opacity', '0.1');
+  var zone = document.getElementById('zone1-3');
+  zone.setAttribute('opacity', '0.1');
+  var zone = document.getElementById('zone1-4');
+  zone.setAttribute('opacity', '0.1');
+  var zone = document.getElementById('path-bars');
+  zone.setAttribute('opacity', '0.5');
+}
+
